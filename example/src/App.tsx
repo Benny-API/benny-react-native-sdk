b4d7c76a1cf91fc8a8080cd96520993534c47210
Steve Austin <steve@bennyapi.com>
2024-04-06 17:57:58 -0400

    Use SafeAreaProvider in sample app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,33 +2,29 @@ import * as React from 'react';
 
 import { EbtBalanceLinkFlow, EbtBalanceLinkFlowEnvironment } from '@bennyapi/react-native-sdk';
 import {
-  Alert, KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet,
+  Alert, KeyboardAvoidingView,
 } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  keyboardAvoiding: {
-    flex: 1,
-  },
-});
+export function EbtBalanceLinkFlowContainer() {
+  const insets = useSafeAreaInsets();
+  return (
+    <KeyboardAvoidingView style={{ flex: 1, paddingTop: insets.top }}>
+      <EbtBalanceLinkFlow
+        organizationId="org_wup29bz683g8habsxvazvyz1"
+        temporaryLink="temp_clr0vujq9000108l66odc7fxv"
+        onExit={() => Alert.alert('onExit called')}
+        onLinkSuccess={(linkToken) => Alert.alert(`onLinkSuccess called ${linkToken}`)}
+        environment={EbtBalanceLinkFlowEnvironment.Sandbox}
+      />
+    </KeyboardAvoidingView>
+  );
+}
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.keyboardAvoiding}
-      >
-        <EbtBalanceLinkFlow
-          organizationId="org_wup29bz683g8habsxvazvyz1"
-          temporaryLink="temp_clr0vujq9000108l66odc7fxv"
-          onExit={() => Alert.alert('onExit called')}
-          onLinkSuccess={(linkToken) => Alert.alert(`onLinkSuccess called ${linkToken}`)}
-          environment={EbtBalanceLinkFlowEnvironment.Sandbox}
-        />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <EbtBalanceLinkFlowContainer />
+    </SafeAreaProvider>
   );
 }
