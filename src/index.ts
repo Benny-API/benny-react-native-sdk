42f450c9920753961fb13f2266bc24220fff9905
Steve Austin <steve@bennyapi.com>
2024-05-07 15:41:16 -0400

    Use paths (#30)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import EbtBalanceFlow from './ebt-transfer/flows/EbtBalanceFlow';
-import EbtTransferFlow from './ebt-transfer/flows/EbtTransferFlow';
-import LinkEbtCardFlow from './ebt-transfer/flows/LinkEbtCardFlow';
-import { EbtTransferEnvironment } from './ebt-transfer/types';
+import EbtBalanceFlow from '@ebt-transfer/flows/EbtBalanceFlow';
+import EbtTransferFlow from '@ebt-transfer/flows/EbtTransferFlow';
+import LinkEbtCardFlow from '@ebt-transfer/flows/LinkEbtCardFlow';
+import { EbtTransferEnvironment } from '@ebt-transfer/types';
 
 export {
   EbtBalanceLinkFlow, EbtBalanceLinkFlowEnvironment,
 } from '@ebt-balance/components/EbtBalanceLinkFlow/EbtBalanceLinkFlow';
+
 export {
   LinkEbtCardFlow, EbtBalanceFlow, EbtTransferFlow, EbtTransferEnvironment,
 };
